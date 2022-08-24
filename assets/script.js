55ab48f03eb611057be7f605b5563ae04ffb1475
jsobie3 <109123352+jsobie3@users.noreply.github.com>
2022-08-23 21:05:12 -0500

    3rd commit

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,3 @@
 console.log('hello');
-
+var searchBtn = document.querySelector('searchBtn')
 var useAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}"
