f4741ef7c543a54e928c2f8dd8dc3ccdd8698844
jsobie3 <109123352+jsobie3@users.noreply.github.com>
2022-08-25 22:33:13 -0500

    City Lookup Correct

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,15 @@
-console.log('hello');
-// var searchBtn = document.querySelector('searchBtn')
-var useAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}"
-var myAPIkey = "6bf91d1a95eef01934c750168d876000"
-
-var city = document.querySelector('#searchBar')
-var citySearch = document.querySelector('#searchBtn')
-
-var name = document.querySelector('.name')
-
-
+var useAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}`
+var myAPIkey = `6bf91d1a95eef01934c750168d876000`
+var city = document.querySelector(`#searchBar`)
+var citySearch = document.querySelector(`#searchBtn`)
+var name = document.querySelector(`.name`)
 function currentTime(){
-    let currentDay = dayjs().format('MMM DD, YYYY:   h:mm a');
-    $('#currentDay').text(currentDay);
+    let currentDay = dayjs().format(`MMM DD, YYYY:   h:mm a`);
+    $(`#currentDay`).text(currentDay);
   }
-
-
 console.log(localStorage)
 function getCoords(search) {
-    var fiveDayForeCast = 'https://api.openweathermap.org/data/2.5/weather?q=' + search + '&units=imperial&appid=' + myAPIkey
-
-
+    var fiveDayForeCast = `https://api.openweathermap.org/data/2.5/weather?q=` + search + `&units=imperial&appid=` + myAPIkey
     fetch(fiveDayForeCast).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
@@ -30,63 +20,53 @@ function getCoords(search) {
         }
     });
 }
-
 function realGetCoords(event) {
     event.preventDefault()
     var search = city.value.trim()
     getCoords(search)
-    weatherDetails()
-    weatherFiveDay()
 }
-
-citySearch.addEventListener('click', realGetCoords)
-
+citySearch.addEventListener(`click`, realGetCoords)
 function weatherDetails(data){
     const name = data.name;
-    const temp = data.main.temp; 
+    const temp = data.main.temp;
     const humidity = data.main.humidity;
     const windSpeed = data.wind.speed;
     console.log(name,temp,humidity,windSpeed)
-
     function displayDetails(){
-        document.getElementById('name').innerText =name;
-        document.getElementById('temp').innerText = temp;
-        document.getElementById('humidity').innerText = humidity;
+        // document.querySelector("#city").textContent = "" + name + " ( " + currentDay +" ) ";
+        document.getElementById(`name`).innerText = "City " + name;
+        document.getElementById(`temp`).innerText = "Temp: " +temp;
+        document.getElementById(`humidity`).innerText = "Humidity: " +humidity;
+       
     }
     displayDetails()
 }
-
-var forcastAPI = 'api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid=' + myAPIkey 
-
+var forcastAPI = `api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid=` + myAPIkey
 function weatherFiveDay (data){
-    
-    const name = data.name;
-
-    const tempDay1 = data.main.temp; 
-    const tempDay2 = data.main.temp; 
-    const tempDay3 = data.main.temp; 
-    const tempDay4 = data.main.temp; 
-    const tempDay5 = data.main.temp; 
-
-    const windSpeedDay1 = data.wind.speed;
-    const windSpeedDay2 = data.wind.speed;
-    const windSpeedDay3 = data.wind.speed;
-    const windSpeedDay4 = data.wind.speed;
-    const windSpeedDay5 = data.wind.speed;
-
-
-    const humidityDay1 = data.main.humidity;
-    const humidityDay2 = data.main.humidity;
-    const humidityDay3 = data.main.humidity;
-    const humidityDay4 = data.main.humidity;
-    const humidityDay5 = data.main.humidity;
-
-    function displayFiveDay(){
-
-        document.getElementById('nameFive').innerText = name;
-        document.getElementById('tempFive').innerText = temp; 
-        document.getElementById('humidityFive').innerText = humidity;
-    
+    // empty out any previous entries
+    $('.fiveday-forecast-container').empty()
+    // loop over the data and call our display five day function
+    for (let i = 0; i < 5; i++) {
+        displayFiveDay(data.daily[i])
     }
-    displayFiveDay()
 }
+
+function displayFiveDay(data) {
+    // get your data similar to your lines 30-33
+    // Below is a template literal - this will help display our data into your 5 day forecast
+    // create cards for each of the 5-day forcast weather
+    const card = `<div class="column col s12 m6 l2">
+    <div class= "card">
+        <div>
+            <ul class="list-group list-group-flush">
+                <h5 class="list-group-item date">${date}</h5>
+                <img class="list-group-item weather-icon" src="http://openweathermap.org/img/wn/${look}@2x.png" alt="">
+                <li class="list-group-item temp">Temp: ${temp}&#8457</li>
+                <li class="list-group-item wind">Wind: ${wind} MPH</li>
+                <li class="list-group-item humidity">Humidity: ${humidity}%</li>
+            </ul>
+        </div>  
+    </div>
+    </div>`;
+    $('.fiveday-forecast-container').append(card)
+  }
